bb7a7cbf9baebc3d82874c5cf978f5ee69f914ee
DavidKocsis0204 <kocsisdavid20020204@gmail.com>
2022-10-12 10:24:56 +0200

    DES form és fileOp elkészítése

---
+++ Amoba/amobaDes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Amoba
+{
+    public partial class amobaDes : Form
+    {
+        public amobaDes()
+        {
+            InitializeComponent();
+        }
+
+        private void amobaDes_Load(object sender, EventArgs e)
+        {
+            Label description = new Label();
+            description.Text=""
+            
+        }
+    }
+}
