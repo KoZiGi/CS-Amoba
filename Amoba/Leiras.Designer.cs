92906fdb89d269ac7edc65c33be7bd126c572e8a
ginalzsolt <ginalzsolt@turr.hu>
2022-10-21 11:09:21 +0200

    működőképes leírás

---
+++ Amoba/Leiras.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Amoba
+{
+    partial class Leiras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Leiras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 195);
+            this.Name = "Leiras";
+            this.Text = "Leiras";
+            this.Load += new System.EventHandler(this.Leiras_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
